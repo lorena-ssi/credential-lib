e7bb7e35e9df1cb3b9c0b56c087bf791778bbed0
Alex Puig <alex@caelumlabs.com>
2020-03-25 12:57:40 +0100

    Credential memberOf

---
+++ src/index.js
@@ -0,0 +1,70 @@
+'use strict'
+let Zenroom = require('@lorena-ssi/zenroom-lib')
+
+// Debug
+var debug = require('debug')('did:debug:matrix')
+var error = require('debug')('did:error:matrix')
+
+const findKey = (obj) => {
+
+}
+/**
+ * Javascript Class to Manage Credentials.
+ */
+module.exports = class Credential {
+  constructor (credential = false) {
+    // Loads the credential we need
+    if (credential !== false) {
+      this.credential = credential
+    }
+    else {
+      this.credential = require('./credentials/memberOf.json')
+    }
+    this.zenroom = new Zenroom()
+  }
+
+  /**
+   * Fills the data for a particular Credential
+   * @param {object} credential The credential to be filled.
+   */
+  fillSubject(credential) {
+    this.credential.credentialSubject['roleName'] = credential.roleName
+    this.credential.credentialSubject.member['id'] = (credential.id ? credential.id : '')
+    this.credential.credentialSubject.member['givenName'] = credential.givenName
+    this.credential.credentialSubject.member['familyName'] = credential.familyName
+    this.credential.credentialSubject.member['additionalName'] = credential.additionalName
+    this.credential.credentialSubject.member['identifier'].value = credential.propertyID
+  }
+
+  /**
+   * Creates the proof for the Credential and returns the Verifiable Credential
+   *  
+   * @param {object} keyPair Keypair to sign with
+   * @param {string} issuer Identity issuing the credential
+   * @param {string} verificationMethod Public Verification method for the signature (if any)
+   */
+  async signCredential(keyPair, issuer, verificationMethod) {
+    const date = new Date()
+    this.credential.issuer = issuer
+    this.credential.issuanceDate = date.toISOString()
+    this.credential.proof.verificationMethod = verificationMethod
+
+    const subject = JSON.stringify(this.credential.credentialSubject)
+    let proof = await this.zenroom.signMessage(issuer, keyPair, subject)
+    proof = JSON.stringify(proof)
+    this.credential.proof.signature = proof
+  }
+
+  /**
+   * Verifies the signature for a credential.
+   */
+  async verifyCredential(issuer, pubKey) {
+    const proof = JSON.parse(this.credential.proof.signature)
+    let publicKey = []
+    publicKey[issuer] = {public_key: pubKey}
+    const check = await this.zenroom.checkSignature(issuer, publicKey, proof, 'verifier')
+    return (check['signature'] === 'correct')
+
+  }
+
+}
---
+++ test/credential.js
@@ -0,0 +1,66 @@
+let Credential = require('../src/index')
+let Zenroom = require('@lorena-ssi/zenroom-lib')
+const assert = require('chai').assert
+let cred = new Credential()
+let zenroom = new Zenroom()
+
+const issuer = 'did:lor:cat:2222'
+const verification = 'https://github.com/.../cat'
+let keypair = false
+
+describe('Credential Object', () => {
+  describe('Basics Credential Object', () => {
+    it('should create a Credential Object', () => {        
+        assert.typeOf(cred, 'object', 'we have an object')
+        assert.typeOf(cred.credential, 'object', 'we have an object')
+    })
+
+    it('should load the Credential JSON', () => {
+        assert.equal(cred.credential['@context'][0], 'https://www.w3.org/2018/credentials/v1')
+        assert.equal(cred.credential['type'][0], 'VerifiableCredential')
+        assert.typeOf(cred.credential.credentialSubject, 'object', 'we have an object')
+    })
+
+    it('should fill the Credential JSON', () => {
+      const credential = {
+        id: 'did:lor:cat:11111',
+        givenName: 'Name1',
+        familyName: 'Last1',
+        additionalName: 'Last2',
+        propertyID: '111111111',
+        telephone: '55523323',
+        roleName: 'volunteer'
+      }
+      cred.fillSubject(credential)
+      let subject = cred.credential.credentialSubject
+      assert.equal(subject['roleName'], credential.roleName)
+      assert.equal(subject.member['id'], credential.id)
+      assert.equal(subject.member['givenName'], credential.givenName)
+      assert.equal(subject.member['familyName'], credential.familyName)
+      assert.equal(subject.member['additionalName'], credential.additionalName)
+      assert.equal(subject.member['identifier'].value, credential.propertyID)
+    })
+
+    it('should sign the Credential', async () => {
+      keypair = await zenroom.newKeyPair(issuer)  
+      await cred.signCredential(keypair, issuer, verification)
+      assert.equal(cred.credential.issuer, issuer,verification)
+      assert.isNotEmpty(cred.credential.issuanceDate)
+      assert.equal(cred.credential.proof.verificationMethod, verification)
+      assert.isNotEmpty(cred.credential.proof.signature)
+    })
+
+    it('should verify the signature of the Credential', async () => {
+      const pubKey = keypair[issuer].keypair.public_key
+      assert.isTrue(await cred.verifyCredential(issuer, pubKey))
+    })
+
+    it('should load a credential and verify the signature', async() => {
+      let testCred = new Credential(cred.credential)
+      const pubKey = keypair[issuer].keypair.public_key
+      assert.isTrue(await testCred.verifyCredential(issuer, pubKey))
+
+    })
+    
+  })
+})
